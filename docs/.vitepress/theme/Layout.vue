9f6bebf64f8ee401d34f7a7ccf04d60fc39d0870
Wang Yun <wangyun1218@gmail.com>
2021-04-05 01:26:40 +0800

    change

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="rounded-t-xl overflow-hidden bg-gradient-to-r from-blue-50 to-blue-100 px-6 py-12"
+  >
+    <div
+      class="max-w-sm mx-auto p-6 flex items-center bg-white rounded-xl shadow-md space-x-4"
+    >
+      <div class="flex-shrink-0">
+        <img class="h-12 w-12" src="./assets/img/logo.svg" alt="ChitChat Logo" />
+      </div>
+      <div>
+        <div class="text-xl font-medium text-black">ChitChat</div>
+        <p class="text-gray-500">You have a new message!</p>
+      </div>
+    </div>
+  </div>
+  <!--<Content /> make sure to include markdown outlet -->
+</template>
+
+<script>
+import './index.css'
+</script>
